1c4f9b4e03fc2215c2f15752342d288886ea336d
Jared Lee <115897648+untamedduck@users.noreply.github.com>
2023-01-15 12:23:04 -0700

    font work

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,7 +11,7 @@ function Navbar(props) {
     }
 
     return (
-        <div className = "flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+        <div className = "flex justify-between items-center h-24 mx-auto px-4 text-[#DDD8AB] bg-[#122223]">
             <div className = "flex shrink-0] ">
                 <img className="h-40 w-40 flex-auto mt-8 " src={BannerImage} alt="Randomizer"/>
             </div>
@@ -25,7 +25,8 @@ function Navbar(props) {
                 {!nav ? <AiOutlineClose size = {20}/> : <AiOutlineMenu size={20}/>}
             </div>
             <div className= "absolute inset-y-0 left-0 z-10">
-                <div className={!nav ? "fixed left-0 top-0  h-full w-[73%] border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%] " }>  {/* Color of mobile menu*/}
+                <div className={!nav ? "fixed left-0 top-0  h-full w-[80%] border-r border-r-gray-900 bg-green-900 " +
+                    "ease-in-out duration-500" : "fixed left-[-100%] " }>  {/* Color of mobile menu*/}
                  <img className="h-40 w-40 pi-5" src={BannerImage} alt="Randomizer"/>
                   <ul className= "uppercase p-2">
                         <li className = "p-4 border-b border-gray-600"> Home </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui',],
+      'serif': ['ui-serif', 'Georgia',],
+      'mono': ['ui-monospace', 'SFMono-Regular',],
+      "Rubik": ["Rubik",]
+    },
+
+      extend: {
+        colors: {
+          'white': '#ffffff',
+          "green": {
+            100: "#3A513A",
+            200: "#8D8741",
+            800: "#122223",
+            900: "#071E07",
+          }
+        },
+        "brown": {
+          100: "#BC986A",
+          200: "#eeac6b",
+          300: "#DDD8AB"
+
+        },
+      },
+    },
+    plugins: [],
+  }
+
