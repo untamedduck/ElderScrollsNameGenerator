b4784fc1e583e2b1beeaef2fa0c10ae703b9bcb1
Jared Lee <untamedduck1@gmail.com>
2023-12-12 17:54:13 -0700

    Updated favicon
    Updated Favicon from the default react one

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import BannerImage from "../Pictures/logo-color.png"
+import BannerImage from "../Pictures/logo-color-removebg.png"
 import{AiOutlineMenu,AiOutlineClose} from "react-icons/ai"
 import {Link} from "react-router-dom"
 
